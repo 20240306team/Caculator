36cb3a8f61e31de96b27f390fbbaedb00933413d
rjadmsehf <jjg524@naver.com>
2024-03-06 16:29:42 +0900

    123

--- untitled4/src/main/java/test/test1.java
+++ untitled4/src/main/java/test/test1.java
@@ -20,13 +20,20 @@ public class test1 {
 
 
 
-        System.out.print("사칙연산 부호를 적으세요 : ");
-        String symbol= scr.nextLine();
-        scr.nextLine();
+
+
+
         System.out.print("첫번째 정수 : ");
         int first = scr.nextInt();
+        scr.nextLine();
+        System.out.print("사칙연산 부호를 적으세요 : ");
+        String symbol= scr.nextLine();
+
         System.out.print("두번째 정수 : ");
         int second = scr.nextInt();
+
+
+
         switch(symbol){
             case "+" :
                 System.out.println(first + second);
@@ -53,7 +60,9 @@ public class test1 {
                 }catch(ArithmeticException e){
                     System.out.println("두번째 정수가 0 이면 안됩니다");
                     break;
-                }
+                } default:
+                System.out.println("잘못 입력했습니다 시스템을 종료합니다.!");
+
         }
 
 
