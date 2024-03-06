51fd454a5b56b113b8e7f318bce79028f9923c3c
leehojun <sciboviator@gmail.com>
2024-03-06 15:15:24 +0900

    더하기

--- untitled4/src/main/java/test/test1.java
+++ untitled4/src/main/java/test/test1.java
@@ -1,10 +1,12 @@
 package test;
 
+import java.util.Scanner;
+
 public class test1 {
 
     public static void main(String[] args) {
         System.out.println("==============================");
-        System.out.println("==                          ==");
+        System.out.println("== 더하기를 하고 싶으면 1을     ==");
         System.out.println("==                          ==");
         System.out.println("==                          ==");
         System.out.println("==                          ==");
@@ -13,5 +15,19 @@ public class test1 {
         System.out.println("==============================");
         System.out.println("==============================");
         System.out.println("==============================");
+
+        Scanner scr = new Scanner(System.in);
+
+        if(scr.nextInt() ==1 ){
+            System.out.println("정수 두 개를 입력하세요");
+            System.out.print("첫번째 정수 : ");
+            int x = scr.nextInt();
+            System.out.print("두번째 정수 : ");
+            int y = scr.nextInt();
+
+            System.out.println(x+" 와 " + y + " 의 합은 "+ (x+y));
+
+        }
+
     }
 }
