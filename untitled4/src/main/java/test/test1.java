e7744084abb4d860b67dd5616af3ae00dff4563a
rjadmsehf <jjg524@naver.com>
2024-03-06 15:12:05 +0900

    123

--- untitled4/src/main/java/test/test1.java
+++ untitled4/src/main/java/test/test1.java
@@ -1,8 +1,12 @@
 package test;
 
+import java.util.Scanner;
+
 public class test1 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("==============================");
         System.out.println("==                          ==");
         System.out.println("==                          ==");
@@ -13,5 +17,15 @@ public class test1 {
         System.out.println("==============================");
         System.out.println("==============================");
         System.out.println("==============================");
+        System.out.println("숫자 하나를 입력해주세요 ");
+        int  a  = sc.nextInt();
+        System.out.println("연산방식을 선택해주세요");
+
+
+
+
+
+
+
     }
 }
